4e112a82c02836c08e0330928476e2cdfcc6b7c9
Mahdi Qiamast <78082316+Qiamast@users.noreply.github.com>
2022-02-12 20:02:34 +0330

    edit word list

--- python/random-word.py
+++ python/random-word.py
@@ -3,8 +3,11 @@ with open("PersianWordList.txt", "r", encoding="utf-8") as f:
   words = f.readlines()
 
 
-rand_word_list = random.choices(words, k=10)
-with open("output.js", 'w', encoding="utf-8") as output:
+rand_word_list = random.choices(words, k=500)
+with open("output.txt", 'w', encoding="utf-8") as output:
   for w in rand_word_list:
     output.write(f"{w}")
   print("done!")
+
+# this script for choise random word list and wirte an new new file
+# @Qiamast
